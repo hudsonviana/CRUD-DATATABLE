b0b62a6ad8d93e761fd34711a90eedd53e85f59b
hudsonviana <hudsonviana@users.noreply.github.com>
2020-04-02 22:30:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -54,8 +54,67 @@
             </div>
         </div>
         <hr class="my-1">
-        
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive" id="mostrarUsuario">
+                    <table class="table table-striped table-sm table-bordered">
+                        <thead>
+                            <tr class="text-center">
+                                <th>ID</th>
+                                <th>Primeiro nome</th>
+                                <th>Último nome</th>
+                                <th>Email</th>
+                                <th>Telefone</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php for($i=1; $i<=100; $i++): ?>
+                                <tr class="text-center text-secondary">
+                                    <td><?php echo $i; ?></td>
+                                    <td>Usuário <?php echo $i; ?></td>
+                                    <td>Teste <?php echo $i; ?></td>
+                                    <td>teste-<?php echo $i; ?>@email.com</td>
+                                    <td>(84) 98851-9954</td>
+                                    <td>
+                                        <a href="#" title="Ver detalhes" class="text-success"><i class="fas fa-info-circle fa-lg"></i></a>&nbsp;&nbsp;
+                                        <a href="#" title="Editar" class="text-primay"><i class="fas fa-edit fa-lg"></i></a>&nbsp;&nbsp;    
+                                        <a href="#" title="Deletar" class="text-danger"><i class="fas fa-trash-alt fa-lg"></i></a>
+                                    </td>
+                                </tr>
+                            <?php endfor; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <!-- Adicionar novo usuário Modal -->
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+        
+            <!-- Modal Header -->
+            <div class="modal-header">
+            <h4 class="modal-title">Modal Heading</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            
+            <!-- Modal body -->
+            <div class="modal-body">
+            Modal body..
+            </div>
+            
+            <!-- Modal footer -->
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            
+        </div>
+        </div>
+    </div>
+
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
@@ -71,7 +130,14 @@
     <!-- DataTable -->
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.js"></script>
 
+    <!-- Sweet Alert 2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('table').DataTable()
+        })
+    </script>
 
 </body>
 </html>
